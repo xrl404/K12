b3f54d8a7644a5dc5fb0ad5274b5c170a9c1b722
xrl404 <leivestadxander@gmail.com>
2026-04-13 15:53:53 -0400

    Update player.js
    why vpn whyyyyy

--- VF_Guide/player.js
+++ VF_Guide/player.js
@@ -613,7 +613,7 @@ async function serverRewards(expressionId, particleName) {
 
   try {
     const [emoteRes, particleRes] = await Promise.all([
-      fetch('https://design-brandon-llp-smooth.trycloudflare.com/webhook/reward-emote', {
+      fetch('https://mechanics-entitled-governance-kit.trycloudflare.com/webhook/reward-emote', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
@@ -622,7 +622,7 @@ async function serverRewards(expressionId, particleName) {
         return r.json();
       }),
 
-      fetch('https://design-brandon-llp-smooth.trycloudflare.com/webhook/play-particle', {
+      fetch('https://mechanics-entitled-governance-kit.trycloudflare.com/webhook/play-particle', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
