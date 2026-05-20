58ea75b80554721f5a0352f3f872507e74948214
xrl404 <leivestadxander@gmail.com>
2026-05-20 14:47:33 -0400

    link again im going to have to do smthing abt this

--- VF_Guide/player.js
+++ VF_Guide/player.js
@@ -626,7 +626,7 @@ async function serverRewards(itemId, particleName) {
 
   try {
     const [emoteRes, particleRes] = await Promise.all([
-      fetch('https://divine-athletic-templates-searched.trycloudflare.com/webhook/grant-inventory-item', {
+      fetch('https://working-emperor-reg-heat.trycloudflare.com/webhook/grant-inventory-item', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
@@ -635,7 +635,7 @@ async function serverRewards(itemId, particleName) {
         return r.json();
       }),
 
-      fetch('https://divine-athletic-templates-searched.trycloudflare.com/webhook/play-particle', {
+      fetch('https://working-emperor-reg-heat.trycloudflare.com/webhook/play-particle', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
